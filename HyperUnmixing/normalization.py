7185243d570e9ccf7a1d5865171b4d661bf806ad
Madeleine Breshears <mdbresh@uw.edu>
2020-05-14 21:07:05 -0700

    fixed typo in return statement

--- HyperUnmixing/normalization.py
+++ HyperUnmixing/normalization.py
@@ -1,29 +1,29 @@
 import numpy as np
 
 def normalization(image,xrange=1):
-    ''' 
-    normalization function for image 
-    ------ 
+    '''
+    normalization function for image
+    ------
     Parameters
-    
-    Image : array 
- 
+
+    Image : array
+
         hyperspectral images with 3D dataset
-    
+
     xrange: output range of the spectra at each point, optional
-        
+
         set to 1
-    
+
     -------
-    Returns 
-    
+    Returns
+
     output : array of float
-       
-       normalized hyperspectral image with 3D dataset 
-    
+
+       normalized hyperspectral image with 3D dataset
+
     ------
     Examples
-    
+
     >>>from HyperUnmixing import normalization
 
     >>>image = np.load('image1.npy')
@@ -31,16 +31,16 @@ def normalization(image,xrange=1):
     >>>plt.plot(image[25,25,:], label='Orginal')
     >>>plt.plot(new_image[25,25,:], label= 'Normalized')
     >>>plt.legend()
-    
+
     '''
-    
+
     x,y,z=image.shape
     new_images=np.zeros((x,y,z))
     count =0
-    
+
     for i in range (0,x):
         for d in range(0,y):
             count +=1
             new_images[i,d,:]=(image[i,d,:]-np.amin(image[i,d,:]))*xrange/(np.amax(image[i,d,:])-np.amin(image[i,d,:]))
-    
-    return new_image
+
+    return new_images
