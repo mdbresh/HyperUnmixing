d636a9acf7d9e966a751351df0f233e080bad496
Preetham Paul <preeth@uw.edu>
2020-06-02 01:08:49 -0700

    Preetham final commit

--- HyperUnmixing/pca_util.py
+++ HyperUnmixing/pca_util.py
@@ -1,10 +1,12 @@
 from sklearn.decomposition import PCA
 import numpy as np
 import matplotlib.pyplot as plt
+from img_util import avg_spectra
 
 def normalize(data):
     """ Normalizes data by shifting origin to mean"""
-    norm_data = data - np.mean(data, axis=0)
+    orig_mean = np.mean(data, axis=0)
+    norm_data = data - orig_mean
     
     return norm_data
 
@@ -14,31 +16,24 @@ def get_PC(im, show_plots=True, top_n=3, PC_n=1, top_load_n=1, figsize=(8,9)):
     get_PC(im)
 
     Returns numpy.ndarray of loading scores for each PC (row) and each feature (column)
+    Also, returns the scree values (Variance shares) for each PC.
     Principal Component Analysis (PCA) gives the significant features for dimentionality reduction
 
     Parameters
     ----------
     im : image passed as numpy array
-    show_plots : True shows plots for analysis, otherwise only loading scores returned
-
-    Arguements if show_plots = True:
-    _
-    top_n : Number of top PCs to plot
-    PC_n : nth PC to show the loading scores
-    top_load_n : Top loading scores of PC_n th PC to be shown in analysis
-    figsize : figsize for plots
 
     Returns
     -------
-    out : ndarray
-        An array object satisfying the specified requirements.
+    out : tuple
+        A tuple of loading scores, scree values and the original mean of data points.
+        This mean of data points is a mean spectrum.
 
     """
     
     #For PCA, each row should be a data point, columns are features
     data = np.reshape(im, (im.shape[0]*im.shape[1], im.shape[2])) #reshaping image - independent pixels
     data = normalize(data)
-    feat_arr = np.arange(750, 750+im.shape[2], 1) #array of features
 
     pca = PCA() #define PCA object
     _ = pca.fit(data) #fit PCA
@@ -46,6 +41,33 @@ def get_PC(im, show_plots=True, top_n=3, PC_n=1, top_load_n=1, figsize=(8,9)):
     scree_values = np.round(pca.explained_variance_ratio_, decimals=5) #Gives scree values array for PCs
     loading_scores = pca.components_ #Loading scores for each feature and each PC
 
+    return (loading_scores, scree_values)
+
+
+def plot_PCs(ax, loading_scores, scree_values, top_n=3, PC_n=1, top_load_n=1):
+    
+    """
+    Updates plt.fig.axes() objects with PCA plots.
+    ax must be an array of 2 axes objects.
+    
+    Parameters
+    -----------------
+    ax : array of 2 plt.fig.axes() objects
+    loading_scores : An array of loading scores (rows are loading scores of each PC and columns are PCs)
+    top_n : Number of top PCs to plot
+    PC_n : nth PC to show the loading scores
+    top_load_n : Top loading scores of PC_n th PC to be shown in analysis
+    
+    Returns
+    ----------------
+    out : array
+        Updated array of axes
+    
+    """
+    
+    
+    feat_arr = np.arange(750, 750+scree_values.shape[0], 1) #array of features
+    
     #Getting top top_load_n number of features in PC_n
     top_inds = np.argsort(loading_scores[PC_n - 1])[-top_load_n:]
     top_feat, top_scores = feat_arr[top_inds], loading_scores[PC_n - 1, top_inds]
@@ -53,7 +75,6 @@ def get_PC(im, show_plots=True, top_n=3, PC_n=1, top_load_n=1, figsize=(8,9)):
     PC_names = ["PC-"+ str(i) for i in np.arange(1,scree_values.shape[0]+1,1)]
 
     #SCREE PLOT : Explained Var./Unexplained Var. ------------------------------------------
-    fig, ax = plt.subplots(nrows = 2, figsize=figsize)
     ax[0].bar(np.arange(1,top_n+1,1), scree_values[:top_n])
     ax[0].set_xticks(np.arange(1,top_n+1,1))
     ax[0].set_xticklabels(PC_names[:top_n])
@@ -75,10 +96,12 @@ def get_PC(im, show_plots=True, top_n=3, PC_n=1, top_load_n=1, figsize=(8,9)):
 
     for i, txt_i in enumerate(top_scores): #Annotate the top features
         ax[1].text(top_feat[i], txt_i, str(round(txt_i,1)), fontsize = 8.5, color = 'black')
+    
+    return ax
+    
 
-    return loading_scores
 
-def make_PC_feats(im_x, loading_scores, PC_num=[1]):
+def make_PC_images(im_x, loading_scores, PC_num=[1]):
     """
     Makes single feature using loading scores of PC_num^th PC, by linear combination of features in im_x
 
@@ -94,9 +117,10 @@ def make_PC_feats(im_x, loading_scores, PC_num=[1]):
         A new x array, with PC as feature in a single column
 
     """
-    new_im_x = np.reshape(np.dot(im_x, loading_scores[PC_num[0]-1]),(-1,1))
+    mean_spectra = avg_spectra(im_x)
+    new_im_x = np.reshape(np.dot(im_x-mean_spectra, loading_scores[PC_num[0]-1]),(-1,1))
     if len(PC_num)>1:
         for PC in PC_num[1:]:
-            new_im_x = np.hstack([new_im_x, np.reshape(np.dot(im_x, loading_scores[PC-1]),(-1,1))])
+            new_im_x = np.hstack([new_im_x, np.reshape(np.dot(im_x-mean_spectra, loading_scores[PC-1]),(-1,1))])
 
-    return new_im_x
+    return np.reshape( new_im_x, (im_x.shape[0], im_x.shape[1], len(PC_num)) )
